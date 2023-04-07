db7e4e8a281efefebddf6301f4b6717bbdae9ca0
arma73 <arma.biz9805@gmail.com>
2023-04-07 15:56:41 +0400

    feat(assy:webp): the initial stage of the binary for parsing scripts

--- packages/@assembly/webpack/cli/wext.ts
+++ packages/@assembly/webpack/cli/wext.ts
@@ -1,14 +1,54 @@
 #!/usr/bin/env node
 
-import { spawn } from "child_process";
+import { resolve } from "path";
+import { program } from "commander";
+import { Log } from "@shared/auxiliaries";
+import { setupBuildCompiler } from "../lib/executes";
+
+import type { Configuration } from "webpack";
+
+const minConf: Configuration = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: resolve(process.cwd(), "build"),
+        filename: "main.js",
+    },
+};
+
+const validOptions = ["build", "dev"];
 
 const spawnScript = () => {
-    const scriptResult = spawn("echo", ["cli is running"]);
+    // Define the "build" command
+    program
+        .command("build")
+        .description("Build the project")
+        .action(() => {
+            // Code to run when the "build" command is used
+            Log.echo("Building the project");
+            setupBuildCompiler(minConf);
+        });
 
-    scriptResult.stdout.on("data", data => {
-        // eslint-disable-next-line no-console
-        console.log(`${data}`);
-    });
+    // Define the "dev" command
+    program
+        .command("dev")
+        .description("Start the development server")
+        .action(() => {
+            // Code to run when the "dev" command is used
+            Log.echo("Starting the development server");
+        });
+
+    program.parse(process.argv);
+
+    const command = program.args[0];
+    if (!validOptions.includes(command)) {
+        console.error(
+            `Invalid command "${command}". Valid options are: ${validOptions.join(
+                ", "
+            )}`
+        );
+        process.exit(1);
+    }
 };
 
 spawnScript();
