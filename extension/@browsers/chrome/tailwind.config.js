013432fe223ab6a12ddd6da51ba30b6a6b7c5cfc
arma73 <arma.biz9805@gmail.com>
2023-04-08 23:01:58 +0400

    chore(ext:chrome): tailwind, watch for shared ui

--- extension/@browsers/chrome/tailwind.config.js
+++ extension/@browsers/chrome/tailwind.config.js
@@ -1,6 +1,12 @@
+const { join } = require("path");
 const baseConfig = require("@config/tailwind").default;
+
+const fromRoot = p => join(__dirname, "..", "..", "..", p);
 
 module.exports = {
     ...baseConfig,
-    content: ["./src/**/*.+(tsx|html)"],
+    content: [
+        "src/**/*.+(tsx|html)",
+        fromRoot("packages/@shared/ui/lib/**/*.+(js|jsx|ts|tsx)"),
+    ],
 };
