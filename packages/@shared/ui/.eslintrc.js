063e79f5c4d06733d1cfdb1dea26c1187eed3310
arma73 <arma.biz9805@gmail.com>
2023-04-08 22:39:44 +0400

    chore(sd:ui): shared ui components package

---
+++ packages/@shared/ui/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "@linter/eslint-config-tsx-prettier",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "tsconfigRootDir": __dirname,
+    },
+};
