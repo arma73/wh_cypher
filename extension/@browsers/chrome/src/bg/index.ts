2a3fe763f929cd814867f0745bb0460437478cb9
arma73 <arma.biz9805@gmail.com>
2023-04-09 15:23:58 +0400

    feat(ext:chrome): open the popup window on installation

--- extension/@browsers/chrome/src/bg/index.ts
+++ extension/@browsers/chrome/src/bg/index.ts
@@ -1,1 +1,7 @@
-export const bg = "bg";
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+    if (reason === "install") {
+        // Open the popup window on installation
+        const url = chrome.runtime.getURL("popup.html");
+        await chrome.tabs.create({ url });
+    }
+});
