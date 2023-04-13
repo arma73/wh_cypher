9126613a07b765e2b851317ad2908c4ab167fe7c
arma73 <arma.biz9805@gmail.com>
2023-04-13 11:59:39 +0400

    feat(ext:chrome): add onInstalled and onMessage listeners
    The ChromeExtensionManager class is instantiated with a configuration object that includes the install property, which specifies the path to the extension's popup HTML file. AccountMessageManager and CryptoMessageManager classes are also instantiated to handle message passing between the background script and the extension's content scripts.

--- extension/@browsers/chrome/src/bg/index.ts
+++ extension/@browsers/chrome/src/bg/index.ts
@@ -1,7 +1,9 @@
-chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-    if (reason === "install") {
-        // Open the popup window on installation
-        const url = chrome.runtime.getURL("popup.html");
-        await chrome.tabs.create({ url });
-    }
-});
+import {
+    ChromeExtensionManager,
+    AccountMessageManager,
+    CryptoMessageManager,
+} from "@shared/models";
+
+new ChromeExtensionManager({ install: { tabPath: "popup.html" } });
+new AccountMessageManager();
+new CryptoMessageManager();
