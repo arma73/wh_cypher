86cac46cca1f6fe253b42ee2085d0e451e146591
arma73 <arma.biz9805@gmail.com>
2023-04-13 12:04:58 +0400

    feat(ext:chrome): wrap app component with redux provider

--- extension/@browsers/chrome/src/app/App.tsx
+++ extension/@browsers/chrome/src/app/App.tsx
@@ -1,11 +1,19 @@
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { setupStore } from "@shared/store";
 
 import "styles/tailwind.css";
 import type { FC } from "react";
 
+const { store } = setupStore();
+
 const App: FC = () => {
-    return <RouterProvider router={router} />;
+    return (
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    );
 };
 
 export default App;
