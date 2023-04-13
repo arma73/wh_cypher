cbae59daecd5fcada596b68d7c706363646ebe01
arma73 <arma.biz9805@gmail.com>
2023-04-14 00:14:42 +0400

    feat(ext:chrome): set height of body to screen size if opener is tab

--- extension/@browsers/chrome/src/app/App.tsx
+++ extension/@browsers/chrome/src/app/App.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { useUIType } from "./hooks";
 import { setupStore } from "@shared/store";
 
 import "styles/tailwind.css";
@@ -9,6 +11,14 @@ import type { FC } from "react";
 const { store } = setupStore();
 
 const App: FC = () => {
+    const uiType = useUIType();
+
+    useEffect(() => {
+        if (uiType === "tab") {
+            document.body.style.height = "100vh";
+        }
+    }, [uiType]);
+
     return (
         <Provider store={store}>
             <RouterProvider router={router} />
