cd15f7f8ec5bad569a3df3d16bf55fef9b13a757
arma73 <arma.biz9805@gmail.com>
2023-04-14 00:27:02 +0400

    feat(ext:chrome): separate register logic with useSingup* hooks and new ui

--- extension/@browsers/chrome/src/app/screens/SignUp.tsx
+++ extension/@browsers/chrome/src/app/screens/SignUp.tsx
@@ -1,93 +1,23 @@
-/* eslint-disable max-lines */
-import { useCallback, useReducer, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate, Form } from "react-router-dom";
-import clsx from "clsx";
-import { cryptoActions } from "@shared/store";
+import { Heading, Input, ThemedButton, Panel } from "@shared/ui";
+import ErrorIcon from "svgr/error.svg";
+import ExtensionLayout from "../layouts/ExtensionLayout";
+import { useInputFocus, useSignupState, useSignupSubmit } from "../hooks";
 
-import type { FC, ChangeEvent, FormEventHandler } from "react";
 import type { IAppState } from "@shared/store";
 
-interface ISignUpProps {
-    className?: string;
-}
+const SignUp = () => {
+    const { inputState, inputDispatch, handlePassword, handleConfirmPassword } =
+        useSignupState();
+    const { handleSubmit } = useSignupSubmit(inputState, inputDispatch);
 
-interface ISignUpState {
-    password: string;
-    confirmPassword: string;
-    error?: string;
-}
-
-type SignUpAction =
-    | { type: "setPassword"; payload: string }
-    | { type: "setConfirmPassword"; payload: string }
-    | { type: "setError"; payload: string };
-
-const initialState: ISignUpState = {
-    password: "",
-    confirmPassword: "",
-};
-
-const signUpReducer = (
-    state: ISignUpState,
-    action: SignUpAction
-): ISignUpState => {
-    switch (action.type) {
-        case "setPassword":
-            return { ...state, password: action.payload };
-        case "setConfirmPassword":
-            return { ...state, confirmPassword: action.payload };
-        case "setError":
-            return { ...state, error: action.payload };
-        default:
-            return state;
-    }
-};
-
-const SignUp: FC<ISignUpProps> = ({ className }) => {
-    const [inputState, inputDispatch] = useReducer(signUpReducer, initialState);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
     const isSaved = useSelector<IAppState, boolean | undefined>(
         state => state.crypto.isSecretSaved
     );
-
-    const handlePassword = ({
-        target: { value },
-    }: ChangeEvent<HTMLInputElement>) => {
-        inputDispatch({ type: "setPassword", payload: value });
-    };
-
-    const handleConfirmPassword = ({
-        target: { value },
-    }: ChangeEvent<HTMLInputElement>) => {
-        inputDispatch({ type: "setConfirmPassword", payload: value });
-    };
-
-    const validatePassword = useCallback((password: string) => {
-        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
-        return pattern.test(password);
-    }, []);
-
-    const onSubmit: FormEventHandler<HTMLFormElement> = () => {
-        if (!validatePassword(inputState.password)) {
-            inputDispatch({
-                type: "setError",
-                payload:
-                    "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.",
-            });
-            return;
-        }
-        if (inputState.password !== inputState.confirmPassword) {
-            inputDispatch({
-                type: "setError",
-                payload: "The confirm password must be equal to the password",
-            });
-            return;
-        }
-
-        dispatch(cryptoActions.saveSecret({ password: inputState.password }));
-    };
+    const inputRef = useInputFocus();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (isSaved) {
@@ -96,33 +26,58 @@ const SignUp: FC<ISignUpProps> = ({ className }) => {
     }, [isSaved, navigate]);
 
     return (
-        <div className={clsx(className)}>
-            <h1>Set password</h1>
-            <Form onSubmit={onSubmit}>
-                <div>
-                    <input
-                        type="password"
-                        name="password"
-                        value={inputState.password}
-                        onChange={handlePassword}
-                        required
+        <ExtensionLayout>
+            <Panel
+                header={<Heading text="Sign up" />}
+                body={
+                    <Form className="w-full px-5">
+                        <div>
+                            <Input
+                                type="password"
+                                name="password"
+                                label="Password"
+                                ref={inputRef}
+                                errIcon={ErrorIcon}
+                                onSubmit={handleSubmit}
+                                className="mb-3"
+                                value={inputState.password}
+                                error={
+                                    inputState.error?.type === "origin"
+                                        ? inputState.error.message
+                                        : false
+                                }
+                                onChange={handlePassword}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <Input
+                                type="password"
+                                name="confirmPassword"
+                                errIcon={ErrorIcon}
+                                onSubmit={handleSubmit}
+                                label="Confirm Password"
+                                value={inputState.confirmPassword}
+                                error={
+                                    inputState.error?.type === "confirm" &&
+                                    inputState.error.message
+                                }
+                                onChange={handleConfirmPassword}
+                                required
+                            />
+                        </div>
+                    </Form>
+                }
+                footer={
+                    <ThemedButton
+                        text="Save"
+                        size="lg"
+                        className="mx-2"
+                        onClick={handleSubmit}
                     />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        value={inputState.confirmPassword}
-                        onChange={handleConfirmPassword}
-                        required
-                    />
-                </div>
-                {inputState.error && <p>{inputState.error}</p>}
-                <div>
-                    <button type="submit">Submitt</button>
-                </div>
-            </Form>
-        </div>
+                }
+            />
+        </ExtensionLayout>
     );
 };
 
